48b3af83eafd5a40bd2aea3795f1ce787a511486
Ahuitzolt <emmanuel.noguez@gmail.com>
2020-07-15 00:09:23 -0500

    Added setup window

--- Pong_game.py
+++ Pong_game.py
@@ -5,4 +5,21 @@ Created on Tue Jul 14 23:43:36 2020
 @author: Emmanuel
 """
 
-import turtle 
+import turtle as t
+
+window = t.Screen()
+
+window.title("Pong game")
+window.bgcolor("black")
+window.setup(width = 800, height = 600)
+window.tracer(0)
+
+
+# Main game loop
+while True:
+    window.update()
+    
+
+
+
+
