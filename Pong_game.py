80d6de0e06b72e9f75784fc92f8be0e350a0ec4d
Ahuitzolt <emmanuel.noguez@gmail.com>
2020-07-17 21:58:05 -0500

    Moved the ball

--- Pong_game.py
+++ Pong_game.py
@@ -39,6 +39,8 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0,0)
+ball.dx = 0.25
+ball.dy = -0.25
 
 # Function 
 def paddle_a_up():
@@ -75,7 +77,31 @@ window.onkeypress(paddle_b_down, "Down")
 while True:
     window.update()
     
-
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+    
+    # Border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+        
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+        
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        
+    if ball.xcor() < -390:
+        ball.goto(0 ,0)
+        ball.dx *= -1
+        
+    
+# Close execute
+t.done()
+t.bye()
 
 
 
